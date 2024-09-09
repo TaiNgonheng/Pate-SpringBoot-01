997164c18682d5ee265a8313ca4d15e98661940e
TaiNgonheng <heng68807@gmail.com>
2024-09-09 22:32:38 +0700

    finish for pate 1

--- Pate-SpringBoot/src/main/java/com/TaiNgonheng/Pate_SpringBoot/currency.java
+++ Pate-SpringBoot/src/main/java/com/TaiNgonheng/Pate_SpringBoot/currency.java
@@ -6,12 +6,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class currency {
 
-
-
-
-
-
-
     public static void main(String[] args) {
         SpringApplication.run(currency.class, args);
     }
